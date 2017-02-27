46e95fe655c7b782e227268ebc894848d649378d
Rikard Lindström <ornotermes@gmail.com>
2017-02-28 00:47:46 +0100

    Added example config.

---
+++ config-example.py
@@ -0,0 +1,24 @@
+#    Copyright 2017 Rikard Lindstrom SA5KPR <ornotermes@gmail.com>
+#
+#    This file is part of APRS-Control.
+#
+#    Foobar is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with APRS-Control.  If not, see <http://www.gnu.org/licenses/>.
+
+myCall = "N0CALL-15"            #Call for this software
+myPass = "12345"                #APRS Password for call
+
+codeCount = 100                 #How many one time codes to generate
+codeLength = 6                  #How long codes you want
+codeCharecters = "adgjmptw"     #Charecters to use in code
+printColumns = 5                #Number of columns when listing codes
