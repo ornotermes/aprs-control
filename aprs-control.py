c3a2816743f0833854c85aee6f2d2c98e2985195
Rikard Lindström <ornotermes@gmail.com>
2017-02-28 00:42:37 +0100

    Added license info in code files.

--- aprs-control.py
+++ aprs-control.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python2
 
+#    Copyright 2017 Rikard Lindstrom SA5KPR <ornotermes@gmail.com>
+#
+#    This file is part of APRS-Control.
+#
+#    Foobar is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with APRS-Control.  If not, see <http://www.gnu.org/licenses/>.
+
 import aprslib
 import logging
 import socket
